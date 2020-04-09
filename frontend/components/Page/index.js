f613550d8066b9b45e3991de3a2df300a63706be
Leonardo Kameya <leonardo.kameya@gmail.com>
2020-04-09 15:15:17 -0400

    Configure Styled-Components
    To make it work with next.js, it must be created a _document.js which it still a class component to inject the styles on the server side

--- frontend/components/Page/index.js
+++ frontend/components/Page/index.js
@@ -1,10 +1,21 @@
 import Header from "../Header";
+import Meta from "../Meta";
+import { StyledButton } from "./styles";
+import { ThemeProvider } from 'styled-components'
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+}
 
 export default function Page({ children }) {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
+      <Meta />
       <Header />
+      <StyledButton>Click Me</StyledButton>
       {children}
-    </div>
+    </ThemeProvider>
   );
 }
