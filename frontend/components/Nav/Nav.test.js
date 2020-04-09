c17470faf4f0e9150f5bce6982966a5c4e67cb40
Leonardo Kameya <leonardo.kameya@gmail.com>
2020-04-09 15:54:12 -0400

    Jest and Enzyme configuration
    Configure jest and enzyme and a snapshot testing example

---
+++ frontend/components/Nav/Nav.test.js
@@ -0,0 +1,11 @@
+import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json';
+import Nav from './index';
+
+describe('<Nav />', () => {
+  it('renders', () => {
+    const wrapper = shallow(<Nav />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+
+});
