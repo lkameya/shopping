929a95b47f38fd5352a37f05da17133ff8977157
Leonardo Kameya <leonardo.kameya@gmail.com>
2020-04-09 23:32:21 -0400

    Feat: Apollo Client configuration
    Configure Apollo Client to retrieve data from GraphQL and produce mutations

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,19 +1,28 @@
-import App from 'next/app';
+import App, { Container } from 'next/app';
 import Page from '../components/Page';
+import { ApolloProvider } from 'react-apollo';
+import withData from '../lib/withData';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, apollo }) {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <Container>
+      <ApolloProvider client={apollo}>
+        <Page>
+          <Component {...pageProps} />
+        </Page>
+      </ApolloProvider>
+    </Container>
   )
 }
 
-// MyApp.getInitialProps = async (appContext) => {
-//   // calls page's `getInitialProps` and fills `appProps.pageProps`
-//   const appProps = await App.getInitialProps(appContext);
-//
-//   return { ...appProps }
-// }
+MyApp.getInitialProps = async ({ Component, ctx }) => {
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  const pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+}
 
-export default MyApp;
+export default withData(MyApp);
